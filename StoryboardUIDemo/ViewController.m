e102126ef25f447e3b63de7717cc0016b8234040
aserbao <1142803753@qq.com>
2021-01-06 18:45:29 +0800

    initialization

--- StoryboardUIDemo/ViewController.m
+++ StoryboardUIDemo/ViewController.m
@@ -13,10 +13,77 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+-(instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil{
+   self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    NSLog(@"%s",__func__);
+    return self;
 }
 
+-(instancetype)init{
+    self = [super init];
+    NSLog(@"%s",__func__);
+    return self;
+}
+
+-(instancetype)initWithCoder:(NSCoder *)aDecoder{
+    self = [super initWithCoder:aDecoder];
+    NSLog(@"%s",__func__);
+    return self;
+}
+
+-(void)awakeFromNib{
+    [super awakeFromNib];
+    NSLog(@"%s",__func__);
+}
+
+-(void)loadView{
+    [super loadView];
+    NSLog(@"%s",__func__);
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"%s",__func__);
+    ViewController *vc = [[ViewController alloc]init];
+    [self.navigationController pushViewController:vc animated:YES];
+}
+
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    NSLog(@"%s",__func__);
+}
+
+-(void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    NSLog(@"%s",__func__);
+}
+
+-(void)viewWillLayoutSubviews{
+    [super viewWillLayoutSubviews];
+    NSLog(@"%s",__func__);
+}
+
+-(void)viewDidLayoutSubviews{
+    [super viewDidLayoutSubviews];
+    NSLog(@"%s",__func__);
+}
+
+-(void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+    NSLog(@"%s",__func__);
+}
+
+-(void)viewDidDisappear:(BOOL)animated{
+    [super viewDidDisappear:animated];
+    NSLog(@"%s",__func__);
+}
+
+-(void)dealloc{
+    NSLog(@"%s",__func__);
+}
 
 @end
+
+
+
