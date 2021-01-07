ee4bca78f3d668ffa5eaa7c66538b2f7e40cfd7d
aserbao <1142803753@qq.com>
2021-01-07 20:53:15 +0800

    initialization

---
+++ AserbaosIOS/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  AserbaosIOS
+//
+//  Created by aserbao on 2021/1/7.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
